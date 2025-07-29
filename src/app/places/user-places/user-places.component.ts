c9af2de4c53895b8b097521679a9f0f397d6af8a
emre1n <emre1n@protonmail.com>
2025-07-29 16:52:20 +0300

    implement http-loaded data via service to user-places instead of a signal in component

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,6 +1,5 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
-import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
@@ -13,19 +12,17 @@ import { PlacesComponent } from '../places.component';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent implements OnInit {
-  places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
 
   private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
 
+  places = this.placesService.loadedUserPlaces;
+
   ngOnInit() {
     this.isFetching.set(true);
     const subscription = this.placesService.loadUserPlaces().subscribe({
-      next: (places) => {
-        this.places.set(places);
-      },
       error: (error: Error) => {
         this.error.set(error.message);
       },
