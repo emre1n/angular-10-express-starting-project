1de27d9e09f943bcf6bbd04e403d1a1c346d2d72
emre1n <emre1n@protonmail.com>
2025-07-30 13:00:11 +0300

    add HTTP interceptors to main ts, it is done where the HTTPClient provided in this case main ts file

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,5 +1,6 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
+import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
@@ -30,6 +31,14 @@ export class UserPlacesComponent implements OnInit {
         this.isFetching.set(false);
       },
     });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
